fbcd6d6283fe682f53923041a9e14647f6547117
Lajos Kossuth <kossuthl4jos@gmail.com>
2025-10-05 17:01:42 +0200

    adjust navbar highlight

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,20 @@
-import { Link } from "@tanstack/react-router";
+import { Link, useRouterState } from "@tanstack/react-router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
 
 export const Navbar = () => {
+  const pathname = useRouterState({
+    select: (state) => {
+      console.log(state);
+      return state.location.pathname;
+    },
+  });
+
   return (
     <div className="fixed bottom-0 left-0 w-full bg-gray-800 text-white grid grid-cols-3 items-center text-center py-3 shadow-[0_-2px_5px_rgba(0,0,0,0.2)]">
       <Link
         className={`flex flex-col items-center justify-center gap-1 transition-colors ${
-          window.location.pathname === "/lists"
+          pathname === "/"
             ? "text-yellow-400"
             : "text-gray-300 hover:text-white"
         }`}
@@ -19,7 +26,7 @@ export const Navbar = () => {
 
       <Link
         className={`flex flex-col items-center justify-center gap-1 transition-colors ${
-          window.location.pathname === "/statistics"
+          pathname === "/statistics"
             ? "text-yellow-400"
             : "text-gray-300 hover:text-white"
         }`}
@@ -31,7 +38,7 @@ export const Navbar = () => {
 
       <Link
         className={`flex flex-col items-center justify-center gap-1 transition-colors ${
-          window.location.pathname === "/recipies"
+          pathname === "/recipies"
             ? "text-yellow-400"
             : "text-gray-300 hover:text-white"
         }`}
--- src/routes/Lists/Lists.tsx
+++ src/routes/Lists/Lists.tsx
@@ -4,7 +4,7 @@ import { getLists } from "../../gateway";
 import { Items, ListManager } from "./components";
 
 export const Lists = () => {
-  const [lists, setLists] = useState<Array<List>>([]);
+  const [lists, setLists] = useState<List[]>([]);
   const [selectedListId, setSelectedListId] = useState<string>();
   const selectedList = lists.find((list) => list._id === selectedListId);
 
